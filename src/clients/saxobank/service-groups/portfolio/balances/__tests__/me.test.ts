2f6aa7bdf25809ecec91514747cf37f9c241aaf8
Niklas Kirk Mouritzsen <niklas@webnuts.com>
2024-10-25 10:33:37 +0200

    Update portfolio endpoint 'me' for balances, based on live data

--- src/clients/saxobank/service-groups/portfolio/balances/__tests__/me.test.ts
+++ src/clients/saxobank/service-groups/portfolio/balances/__tests__/me.test.ts
@@ -2,11 +2,25 @@ import { describe, expect, test } from '../../../../../../utils/testing.ts'
 import { SaxoBankApplication } from '../../../../../saxobank-application.ts'
 
 describe('portfolio/balances/me', () => {
-  test('response passes guard', async () => {
-    using app = new SaxoBankApplication()
+  describe('live', () => {
+    using appLive = new SaxoBankApplication({
+      type: 'Live',
+    })
 
-    const me = await app.portfolio.balances.me.get()
+    test('response passes guard', async () => {
+      const me = await appLive.portfolio.balances.me.get()
+      expect(me).toBeDefined()
+    })
+  })
 
-    expect(me).toBeDefined()
+  describe('simulation', () => {
+    using appSimulation = new SaxoBankApplication({
+      type: 'Simulation',
+    })
+
+    test('response passes guard', async () => {
+      const me = await appSimulation.portfolio.balances.me.get()
+      expect(me).toBeDefined()
+    })
   })
 })
