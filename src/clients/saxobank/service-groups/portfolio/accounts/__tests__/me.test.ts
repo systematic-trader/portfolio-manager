0b67bc8c0e932063ae90105c1394de703a155511
Niklas Kirk Mouritzsen <niklas@webnuts.com>
2024-10-25 10:17:41 +0200

    Update portfolio endpoint 'me' for accounts, based on live data

--- src/clients/saxobank/service-groups/portfolio/accounts/__tests__/me.test.ts
+++ src/clients/saxobank/service-groups/portfolio/accounts/__tests__/me.test.ts
@@ -3,11 +3,25 @@ import { describe, expect, test } from '../../../../../../utils/testing.ts'
 import { SaxoBankApplication } from '../../../../../saxobank-application.ts'
 
 describe('portfolio/accounts/me', () => {
-  test('response passes guard', async () => {
-    using app = new SaxoBankApplication()
+  describe('live', () => {
+    using appLive = new SaxoBankApplication({
+      type: 'Live',
+    })
 
-    const [me] = await toArray(app.portfolio.accounts.me.get())
+    test('response passes guard', async () => {
+      const [me] = await toArray(appLive.portfolio.accounts.me.get())
+      expect(me).toBeDefined()
+    })
+  })
 
-    expect(me).toBeDefined()
+  describe('simulation', () => {
+    using appSimulation = new SaxoBankApplication({
+      type: 'Simulation',
+    })
+
+    test('response passes guard', async () => {
+      const [me] = await toArray(appSimulation.portfolio.accounts.me.get())
+      expect(me).toBeDefined()
+    })
   })
 })
--- src/clients/saxobank/types/records/account-response.ts
+++ src/clients/saxobank/types/records/account-response.ts
@@ -28,31 +28,34 @@ export const AccountResponse = props({
   /** Unique ID of the account, displayed to the user */
   AccountId: string(),
 
-  /** Unique ID of the account. */
+  /** Unique ID of the account */
   AccountKey: string(),
 
-  /** Sub type of the account. */
+  /** Sub type of the account */
   AccountSubType: AccountSubType,
 
-  /** Type of the account. */
+  /** Type of the account */
   AccountType: AccountType,
 
-  /** If set, this value shields the account value from going below the given limit by automatically triggering closing of positions should the limit be exceeded. A limit of zero means there is no limit. */
+  /**
+   * If set, this value shields the account value from going below the given limit by automatically triggering closing of positions should the limit be exceeded.
+   * A limit of zero means there is no limit
+   */
   AccountValueProtectionLimit: optional(number()),
 
-  /** Indicates whether the account is active or not. */
+  /** Indicates whether the account is active or not */
   Active: boolean(),
 
-  /** If the account follows a trade leader on AutoTrading/SaxoSelect, this is the leader identifier. */
+  /** If the account follows a trade leader on AutoTrading/SaxoSelect, this is the leader identifier */
   AutoTradingInvestmentId: optional(integer()),
 
-  /** Contains the instrument to be used as comparison then evaluating account performance. */
+  /** Contains the instrument to be used as comparison then evaluating account performance */
   BenchmarkInstrument: optional(AccountBenchmarkInstrument),
 
-  /** If true, the user may enable/disable the use of cash positions as margin trading collateral on the given account. */
+  /** If true, the user may enable/disable the use of cash positions as margin trading collateral on the given account */
   CanUseCashPositionsAsMarginCollateral: boolean(),
 
-  /** Indicates if the account is configured for CFD borrowing costs. */
+  /** Indicates if the account is configured for CFD borrowing costs */
   CfdBorrowingCostsActive: optional(boolean()),
 
   /** Unique ID of the client owning the account */
@@ -61,70 +64,85 @@ export const AccountResponse = props({
   /** Unique ID of the client - for navigation purposes */
   ClientKey: string(),
 
-  /** The UTC date and time the account was created. */
+  /** The UTC date and time the account was created */
   CreationDate: string({ format: 'date-iso8601' }),
 
-  /** Account currency. */
+  /** Account currency */
   Currency: Currency3,
 
-  /** Number of decimals used in currency. */
+  /** Number of decimals used in currency */
   CurrencyDecimals: integer(),
 
-  /** Indicates direct market access for the account. */
+  /** Indicates direct market access for the account */
   DirectMarketAccess: boolean(),
 
-  /** Lists exchanges for which the account is direct market access enabled. */
+  /** Lists exchanges for which the account is direct market access enabled */
   DirectMarketExchangesIds: optional(array(string())),
 
-  /** User customizable account name. */
+  /** User customizable account name */
   DisplayName: optional(string()),
 
-  /** Indicates whether (and through which provider) dividend reinvestment is enabled for this account. If dividend reinvestment is not enabled, this field is omitted from the response, in which case dividends are booked as cash on the account. */
+  /**
+   * Indicates whether (and through which provider) dividend reinvestment is enabled for this account.
+   * If dividend reinvestment is not enabled, this field is omitted from the response, in which case dividends are booked as cash on the account.
+   */
   DividendReinvestmentConfig: optional(DividendReinvestmentConfiguration),
 
-  /** FractionalOrder is enabled or not for the account. */
+  /** FractionalOrder is enabled or not for the account */
   FractionalOrderEnabled: boolean(),
 
   /* Not documented */
   FractionalOrderEnabledAssetTypes: optional(array(AssetType)),
 
-  /** Indicates that the margin exposure is calculated for this account only, without cross margining to other accounts within the same account group. */
+  /** Indicates that the margin exposure is calculated for this account only, without cross margining to other accounts within the same account group */
   IndividualMargining: boolean(),
 
-  /** If true, currency conversions between trade and account currency take place at time of trade settlement. Typically end of business day. */
+  /**
+   * If true, currency conversions between trade and account currency take place at time of trade settlement
+   * Typically end of business day
+   */
   IsCurrencyConversionAtSettlementTime: boolean(),
 
-  /** Indicates whether trading on margin is allowed for the account. */
+  /** Indicates whether trading on margin is allowed for the account */
   IsMarginTradingAllowed: boolean(),
 
-  /** Indicates whether or not the account can be shared. An account can only be shared if it is the only account of its client and it is not a trial account. */
+  /**
+   * Indicates whether or not the account can be shared.
+   * An account can only be shared if it is the only account of its client and it is not a trial account
+   */
   IsShareable: boolean(),
 
-  /** IsTrialAccount - Is this a trial account. */
+  /** IsTrialAccount - Is this a trial account */
   IsTrialAccount: boolean(),
 
-  /** AssetTypes that can be traded on this account. */
+  /** AssetTypes that can be traded on this account */
   LegalAssetTypes: array(AssetType),
 
-  /** Account's Management Type. */
+  /** Account's Management Type */
   ManagementType: ManagementType,
 
-  /** Calculation method for assessing margin utilization. */
+  /** Calculation method for assessing margin utilization */
   MarginCalculationMethod: PortfolioMarginMethod,
 
-  /** Margin Lending Enabled. */
+  /** Margin Lending Enabled */
   MarginLendingEnabled: MarginLendingEnabled,
 
-  /** Portfolio Based Margin (PBM) is a method for mapping the risk of an investment portfolio. True if enabled else false. */
+  /**
+   * Portfolio Based Margin (PBM) is a method for mapping the risk of an investment portfolio.
+   * True if enabled else false
+   */
   PortfolioBasedMarginEnabled: boolean(),
 
-  /** Returns a list of application identifiers that the account has been shared with. */
+  /** Returns a list of application identifiers that the account has been shared with */
   Sharing: array(string()),
 
-  /** If true, an AccountValueProtectionLimit may be set on this account. If it is false, the AccountValueProtectionLimit can be set on client or account group. */
-  SupportsAccountValueProtectionLimit: optional(boolean()),
+  /**
+   * If true, an AccountValueProtectionLimit may be set on this account.
+   * If it is false, the AccountValueProtectionLimit can be set on client or account group
+   */
+  SupportsAccountValueProtectionLimit: boolean(),
 
-  /** Enable/disable the use of cash positions for margin trading collateral. */
+  /** Enable/disable the use of cash positions for margin trading collateral */
   UseCashPositionsAsMarginCollateral: boolean(),
 
   /** Not documented */
