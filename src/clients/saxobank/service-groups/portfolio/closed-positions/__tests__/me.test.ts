40f23f4008c322b80030e092b8b8deec1923be35
Niklas Kirk Mouritzsen <niklas@webnuts.com>
2024-10-25 10:39:31 +0200

    Add live and sim test for closed-positions (but maybe we should remove it, since it requires end of day netting mode

--- src/clients/saxobank/service-groups/portfolio/closed-positions/__tests__/me.test.ts
+++ src/clients/saxobank/service-groups/portfolio/closed-positions/__tests__/me.test.ts
@@ -2,12 +2,27 @@ import { toArray } from '../../../../../../utils/async-iterable.ts'
 import { describe, expect, test } from '../../../../../../utils/testing.ts'
 import { SaxoBankApplication } from '../../../../../saxobank-application.ts'
 
-describe('portfolio/closedPositions/me', () => {
-  test('response passes guard', async () => {
-    using app = new SaxoBankApplication()
+// todo this endpoint can only be used if the accounts netting mode is set to EndOfDay
+describe('portfolio/closed-positions/me', () => {
+  describe('live', () => {
+    using appLive = new SaxoBankApplication({
+      type: 'Live',
+    })
 
-    const [me] = await toArray(app.portfolio.closedPositions.me.get())
+    test('response passes guard', async () => {
+      const [me] = await toArray(appLive.portfolio.closedPositions.me.get())
+      expect(me).toBeDefined()
+    })
+  })
 
-    expect(me).toBeDefined()
+  describe('simulation', () => {
+    using appSimulation = new SaxoBankApplication({
+      type: 'Simulation',
+    })
+
+    test('response passes guard', async () => {
+      const [me] = await toArray(appSimulation.portfolio.closedPositions.me.get())
+      expect(me).toBeDefined()
+    })
   })
 })
