55dd122b2ac4263cbd712d065b9af92253485b04
Niklas Kirk Mouritzsen <niklas@webnuts.com>
2024-10-29 09:30:35 +0100

    Add a simulation-test for currency exposure

--- src/clients/saxobank/service-groups/portfolio/exposure/currency/__tests__/me.test.ts
+++ src/clients/saxobank/service-groups/portfolio/exposure/currency/__tests__/me.test.ts
@@ -1,5 +1,6 @@
-import { describe, expect, test } from '../../../../../../../utils/testing.ts'
+import { afterAll, beforeEach, describe, expect, test } from '../../../../../../../utils/testing.ts'
 import { SaxoBankApplication } from '../../../../../../saxobank-application.ts'
+import { TestingUtilities } from '../../../../../__tests__/testing-utilities.ts'
 
 describe('portfolio/exposure/currency/me', () => {
   describe('live', () => {
@@ -8,8 +9,8 @@ describe('portfolio/exposure/currency/me', () => {
     })
 
     test('response passes guard', async () => {
-      const me = await appLive.portfolio.exposure.currency.me.get()
-      expect(me).toBeDefined()
+      const exposure = await appLive.portfolio.exposure.currency.me.get()
+      expect(exposure).toBeDefined()
     })
   })
 
@@ -18,9 +19,45 @@ describe('portfolio/exposure/currency/me', () => {
       type: 'Simulation',
     })
 
-    test('response passes guard', async () => {
-      const me = await appSimulation.portfolio.exposure.currency.me.get()
-      expect(me).toBeDefined()
+    const { resetSimulationAccount, waitForOrderCount } = new TestingUtilities({ app: appSimulation })
+
+    beforeEach(resetSimulationAccount)
+    afterAll(resetSimulationAccount)
+
+    test('response passes guard, with no orders or positions', async () => {
+      const exposure = await appSimulation.portfolio.exposure.currency.me.get()
+      expect(exposure).toBeDefined()
+    })
+
+    test('response passes guard, with an open FxSpot position', async () => {
+      const initialExposure = await appSimulation.portfolio.exposure.currency.me.get()
+      expect(initialExposure).toBeDefined()
+
+      const initialCurrencyExposure = initialExposure.map(({ Currency }) => Currency)
+      expect(initialCurrencyExposure).toHaveLength(1)
+      expect(initialCurrencyExposure).toContain('EUR')
+
+      await appSimulation.trade.orders.post({
+        AssetType: 'FxSpot',
+        BuySell: 'Buy',
+        Amount: 50_000,
+        OrderType: 'Market',
+        OrderDuration: { DurationType: 'DayOrder' },
+        ManualOrder: false,
+        Uic: 21, // EUR/USD
+        RequestId: crypto.randomUUID(),
+        ExternalReference: crypto.randomUUID(),
+      })
+
+      await waitForOrderCount({ count: 0 })
+
+      const updatedExposure = await appSimulation.portfolio.exposure.currency.me.get()
+      expect(updatedExposure).toBeDefined()
+
+      const currencies = updatedExposure.map(({ Currency }) => Currency)
+      expect(currencies).toHaveLength(2)
+      expect(currencies).toContain('USD')
+      expect(currencies).toContain('EUR')
     })
   })
 })
