3885f41f7157bfff0b5690a6a288d864773e3fa2
Niklas Kirk Mouritzsen <niklas@webnuts.com>
2024-10-30 08:53:13 +0100

    Add test which places market orders on closed exchanges to test the orders endpoint

--- src/clients/saxobank/service-groups/portfolio/orders/__tests__/me.test.ts
+++ src/clients/saxobank/service-groups/portfolio/orders/__tests__/me.test.ts
@@ -1,6 +1,7 @@
 import { toArray } from '../../../../../../utils/async-iterable.ts'
-import { describe, expect, test } from '../../../../../../utils/testing.ts'
+import { afterAll, beforeEach, describe, expect, test } from '../../../../../../utils/testing.ts'
 import { SaxoBankApplication } from '../../../../../saxobank-application.ts'
+import { TestingUtilities } from '../../../../__tests__/testing-utilities.ts'
 
 describe('portfolio/orders/me', () => {
   describe('live', () => {
@@ -14,10 +15,51 @@ describe('portfolio/orders/me', () => {
     })
   })
 
-  // todo place a limit order and test the reponse (we can't use market orders, since they might be filled)
   describe('simulation', () => {
     using appSimulation = new SaxoBankApplication({
       type: 'Simulation',
+    })
+
+    const { findTradableInstruments, resetSimulationAccount } = new TestingUtilities({ app: appSimulation })
+
+    beforeEach(resetSimulationAccount)
+    afterAll(resetSimulationAccount)
+
+    test('response passes guard for market stock orders', async ({ step }) => {
+      const instruments = findTradableInstruments({
+        assetTypes: ['Stock'],
+        limit: 100,
+        sessions: ['Closed'],
+      })
+
+      let count = 0
+      for await (const instrument of instruments) {
+        await step(`Placing a market order for ${instrument.Description} (UIC ${instrument.Uic})`, async () => {
+          const placeOrderResponse = await appSimulation.trade.orders.post({
+            AssetType: 'Stock',
+            Amount: 1,
+            BuySell: 'Buy',
+            ManualOrder: false,
+            OrderType: 'Market',
+            OrderDuration: { DurationType: 'DayOrder' },
+            ExternalReference: crypto.randomUUID(),
+            Uic: instrument.Uic,
+          })
+          expect(placeOrderResponse).toBeDefined()
+
+          const orders = await toArray(appSimulation.portfolio.orders.me.get())
+          expect(orders).toBeDefined()
+          expect(orders).toHaveLength(1)
+
+          await resetSimulationAccount()
+        })
+
+        count++
+      }
+
+      if (count === 0) {
+        throw new Error('Failed to find any instruments to base the test on')
+      }
     })
 
     test('response passes guard', async () => {
