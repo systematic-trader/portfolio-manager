89c35c85a9d4c6a7f10b5d06b78c9b383ef19cff
Jacob Madsen <jacob@webnuts.com>
2024-11-18 10:31:45 +0100

    Changed to rest parameter

--- src/utils/signal.ts
+++ src/utils/signal.ts
@@ -1,43 +1,41 @@
-export function mergeAbortSignals(
-  signal1: undefined | AbortSignal,
-  signal2: undefined | AbortSignal,
-): undefined | AbortSignal {
-  if (signal1 === undefined) {
-    return signal2
+export function mergeAbortSignals(...signals: ReadonlyArray<undefined | AbortSignal>): undefined | AbortSignal {
+  if (signals.length === 0) {
+    return undefined
   }
 
-  if (signal2 === undefined) {
-    return signal1
-  }
+  let merge = false
 
-  if (signal1.aborted === true) {
-    return signal1
-  }
-
-  if (signal2.aborted === true) {
-    return signal2
-  }
-
-  const mergedController = new AbortController()
-
-  const listener1 = () => {
-    if (mergedController.signal.aborted === false) {
-      mergedController.abort()
+  for (const signal of signals) {
+    if (signal === undefined) {
+      continue
     }
 
-    signal1.removeEventListener('abort', listener1)
-  }
-
-  const listener2 = () => {
-    if (mergedController.signal.aborted === false) {
-      mergedController.abort()
+    if (signal.aborted) {
+      return signal
     }
 
-    signal2.removeEventListener('abort', listener2)
+    merge = true
   }
 
-  signal1.addEventListener('abort', listener1)
-  signal2.addEventListener('abort', listener2)
+  if (merge === false) {
+    return undefined
+  }
 
-  return mergedController.signal
+  const controller = new AbortController()
+
+  for (const signal of signals) {
+    if (signal === undefined || signal.aborted) {
+      continue
+    }
+
+    const listener = () => {
+      if (controller.signal.aborted === false) {
+        controller.abort()
+      }
+    }
+
+    signal.addEventListener('abort', listener, { once: true })
+  }
+
+  return controller.signal
 }
