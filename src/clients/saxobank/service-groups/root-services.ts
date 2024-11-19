cc1f1a1f0bda9f7fa9a59d818e0b518c7aa7476e
Niklas Kirk Mouritzsen <niklas@webnuts.com>
2024-11-19 11:15:29 +0100

    Integrate endpoint root/v1/subscriptions

--- src/clients/saxobank/service-groups/root-services.ts
+++ src/clients/saxobank/service-groups/root-services.ts
@@ -1,12 +1,15 @@
 import type { ServiceGroupClient } from '../service-group-client.ts'
 import { IsAlive } from './root-services/is-alive.ts'
+import { Subscriptions } from './root-services/subscriptions.ts'
 
 export class RootServices {
   readonly isAlive: IsAlive
+  readonly subscriptions: Subscriptions
 
   constructor({ client }: { readonly client: ServiceGroupClient }) {
     const serviceGroupClient = client.appendPath('root')
 
     this.isAlive = new IsAlive({ client: serviceGroupClient })
+    this.subscriptions = new Subscriptions({ client: serviceGroupClient })
   }
 }
