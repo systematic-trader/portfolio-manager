f7930f7b16d360f82de79260df51a3c6151c839c
Niklas Kirk Mouritzsen <niklas@webnuts.com>
2024-11-19 11:15:29 +0100

    Integrate endpoint root/v1/subscriptions

---
+++ src/clients/saxobank/service-groups/root-services/subscriptions.ts
@@ -0,0 +1,31 @@
+import type { ServiceGroupClient } from '../../service-group-client.ts'
+
+export class Subscriptions {
+  readonly #client: ServiceGroupClient
+
+  constructor({ client }: { readonly client: ServiceGroupClient }) {
+    this.#client = client.appendPath('v1/subscriptions')
+  }
+
+  /**
+   * Removes multiple subscriptions for the current session, and frees all resources on the server.
+   */
+  async delete(
+    { ContextId, Tag }: {
+      /** Unique streaming context ID part of the streaming session */
+      readonly ContextId: string
+
+      /** Tag that subscriptions are marked with */
+      readonly Tag?: undefined | string
+    },
+    options: { readonly timeout?: undefined | number } = {},
+  ): Promise<void> {
+    return await this.#client.delete({
+      path: ContextId,
+      searchParams: {
+        Tag,
+      },
+      timeout: options.timeout,
+    })
+  }
+}
