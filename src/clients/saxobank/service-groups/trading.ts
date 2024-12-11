680b3ec3ff69b9ceace99dfde8eb85bb4367f5ce
Niklas Kirk Mouritzsen <niklas@webnuts.com>
2024-12-11 10:50:28 +0100

    Implement price-subscription endpoint, based on live data

--- src/clients/saxobank/service-groups/trading.ts
+++ src/clients/saxobank/service-groups/trading.ts
@@ -3,19 +3,22 @@ import { InfoPrices } from './trading/info-prices.ts'
 import { IsAlive } from './trading/is-alive.ts'
 import { Orders } from './trading/orders.ts'
 import { Positions } from './trading/positions.ts'
+import { Prices } from './trading/prices.ts'
 
 export class Trading {
+  readonly infoPrices: InfoPrices
   readonly isAlive: IsAlive
   readonly orders: Orders
   readonly positions: Positions
-  readonly infoPrices: InfoPrices
+  readonly prices: Prices
 
   constructor({ client }: { readonly client: ServiceGroupClient }) {
     const serviceGroupClient = client.appendPath('trade')
 
+    this.infoPrices = new InfoPrices({ client: serviceGroupClient })
     this.isAlive = new IsAlive({ client: serviceGroupClient })
     this.orders = new Orders({ client: serviceGroupClient })
     this.positions = new Positions({ client: serviceGroupClient })
-    this.infoPrices = new InfoPrices({ client: serviceGroupClient })
+    this.prices = new Prices({ client: serviceGroupClient })
   }
 }
---
+++ src/clients/saxobank/types/records/margin-impact-buy-sell.ts
@@ -0,0 +1,115 @@
+import {
+  type GuardType,
+  number,
+  props,
+} from 'https://raw.githubusercontent.com/systematic-trader/type-guard/main/mod.ts'
+import { Currency3 } from '../derives/currency.ts'
+
+export const MarginImpactBuySell = props({
+  /** Currency for margin impacts */
+  Currency: Currency3,
+
+  /** The initial margin available if instrument is bought */
+  InitialMarginAvailableBuy: number(),
+
+  /** The current initial margin available */
+  InitialMarginAvailableCurrent: number(),
+
+  /** The initial margin available if instrument is sold */
+  InitialMarginAvailableSell: number(),
+
+  /** The initial margin to pay if instrument is bought */
+  InitialMarginBuy: number(),
+
+  /** The initial margin to pay if instrument is sold */
+  InitialMarginSell: number(),
+
+  /** The initial spending power available */
+  InitialSpendingPower: number(),
+
+  /** The maintenance margin to pay if instrument is bought */
+  MaintenanceMarginBuy: number(),
+
+  /** The maintenance margin to pay if instrument is sold */
+  MaintenanceMarginSell: number(),
+
+  /** The maintenance spending power available */
+  MaintenanceSpendingPower: number(),
+
+  /** The margin to pay if instrument is bought */
+  MarginBuy: number(),
+
+  /** The margin to pay if instrument is sold */
+  MarginSell: number(),
+
+  /** Not documented. */
+  CollateralUtilizationAfterTradeBuy: number(),
+
+  /** Not documented. */
+  CollateralUtilizationAfterTradeSell: number(),
+
+  /** Not documented. */
+  CurrentCollateralDeductionFromRiskConcentration: number(),
+
+  /** Not documented. */
+  EstimatedCollateralDeductionFromRiskConcentration: number(),
+
+  /** Not documented. */
+  InitialCollateralAvailableBuy: number(),
+
+  /** Not documented. */
+  InitialCollateralAvailableCurrent: number(),
+
+  /** Not documented. */
+  InitialCollateralAvailableSell: number(),
+
+  /** Not documented. */
+  InitialCollateralBuy: number(),
+
+  /** Not documented. */
+  InitialCollateralSell: number(),
+
+  /** Not documented. */
+  InitialMarginAndLoanUtilizationBuy: number(),
+
+  /** Not documented. */
+  InitialMarginAndLoanUtilizationSell: number(),
+
+  /** Not documented. */
+  InitialMarginUtilizationPercentBuy: number(),
+
+  /** Not documented. */
+  InitialMarginUtilizationPercentSell: number(),
+
+  /** Not documented. */
+  MaintenanceMarginAndLoanUtilizationBuy: number(),
+
+  /** Not documented. */
+  MaintenanceMarginAndLoanUtilizationSell: number(),
+
+  /** Not documented. */
+  MaintenanceMarginUtilizationPercentBuy: number(),
+
+  /** Not documented. */
+  MaintenanceMarginUtilizationPercentSell: number(),
+
+  /** Not documented. */
+  MarginAndCollateralUtilizationAfterTradeBuy: number(),
+
+  /** Not documented. */
+  MarginAndCollateralUtilizationAfterTradeSell: number(),
+
+  /** Not documented. */
+  MarginAndLoanUtilizationAfterTradeBuy: number(),
+
+  /** Not documented. */
+  MarginAndLoanUtilizationAfterTradeSell: number(),
+
+  /** Not documented. */
+  MarginUtilizationPercentBuy: number(),
+
+  /** Not documented. */
+  MarginUtilizationPercentSell: number(),
+})
+
+export type MarginImpactBuySell = GuardType<typeof MarginImpactBuySell>
