145f570f12c23099eac41924f3dc0269dc3c4047
Niklas Kirk Mouritzsen <niklas@webnuts.com>
2024-12-18 09:28:48 +0100

    Initial implementation of cash-management endpoints (currently responds with 403)

--- src/clients/saxobank/service-groups/client-services.ts
+++ src/clients/saxobank/service-groups/client-services.ts
@@ -1,14 +1,17 @@
 import type { ServiceGroupClient } from '../service-group-client/service-group-client.ts'
+import { CashManagement } from './client-services/cash-management.ts'
 import { IsAlive } from './client-services/is-alive.ts'
 import { TradingConditions } from './client-services/trading-conditions.ts'
 
 export class ClientServices {
+  readonly cashManagement: CashManagement
   readonly isAlive: IsAlive
   readonly tradingConditions: TradingConditions
 
   constructor({ client }: { readonly client: ServiceGroupClient }) {
     const serviceGroupClient = client.appendPath('cs')
 
+    this.cashManagement = new CashManagement({ client: serviceGroupClient })
     this.isAlive = new IsAlive({ client: serviceGroupClient })
     this.tradingConditions = new TradingConditions({ client: serviceGroupClient })
   }
