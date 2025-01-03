70aedfc84e590fc62387d699135f40e2c5225d0a
Niklas Kirk Mouritzsen <niklas@webnuts.com>
2025-01-03 08:56:10 +0100

    Remove cash management validations endpoint (we dont have permissions to use it)

--- src/clients/saxobank/service-groups/client-services/cash-management.ts
+++ src/clients/saxobank/service-groups/client-services/cash-management.ts
@@ -1,15 +1,12 @@
 import type { ServiceGroupClient } from '../../service-group-client/service-group-client.ts'
 import { InterAccountTransfers } from './cash-management/inter-account-transfers.ts'
-import { Validations } from './cash-management/validations.ts'
 
 export class CashManagement {
   readonly interAccountTransfers: InterAccountTransfers
-  readonly validations: Validations
 
   constructor({ client }: { readonly client: ServiceGroupClient }) {
     const serviceGroupClient = client.appendPath('v2/cashmanagement')
 
     this.interAccountTransfers = new InterAccountTransfers({ client: serviceGroupClient })
-    this.validations = new Validations({ client: serviceGroupClient })
   }
 }
