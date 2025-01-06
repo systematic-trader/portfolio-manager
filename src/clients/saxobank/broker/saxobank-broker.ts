bb2468bbba2726bc59b844086ce74183b82c44e3
Jacob Madsen <jacob@webnuts.com>
2025-01-06 11:47:29 +0100

    Removed name from broker internal state

--- src/clients/saxobank/broker/saxobank-broker.ts
+++ src/clients/saxobank/broker/saxobank-broker.ts
@@ -80,7 +80,6 @@ export async function SaxoBankBroker<const Options extends SaxoBankBrokerOptions
 
         return {
           clientKey: value.ClientKey,
-          name: value.Name,
         }
       })
     })
