a3768c4ac362da8fdcd6dbc242ae23e6f79a8e04
Jacob Madsen <jacob@webnuts.com>
2025-03-13 18:17:55 +0100

    WIP

--- src/clients/interactive-brokers/resources/iserver/secdef/info.ts
+++ src/clients/interactive-brokers/resources/iserver/secdef/info.ts
@@ -1,6 +1,83 @@
+import {
+  boolean,
+  type GuardType,
+  literal,
+  number,
+  pattern,
+  props,
+  string,
+  union,
+} from 'https://raw.githubusercontent.com/systematic-trader/type-guard/main/mod.ts'
 import type { InteractiveBrokersResourceClient } from '../../../resource-client.ts'
-import type { AssetClass } from '../../../types/derived/asset-class.ts'
-import type { ExchangeCode } from '../../../types/derived/exchange-code.ts'
+import { AssetClass } from '../../../types/derived/asset-class.ts'
+import { Currency3 } from '../../../types/derived/currency.ts'
+import { ExchangeCode } from '../../../types/derived/exchange-code.ts'
+
+export const BondSecurityInfo = props({
+  companyName: string(),
+  conid: number(),
+  currency: Currency3,
+  exchange: ExchangeCode,
+  maturityDate: pattern(/^\d{8}$/), // YYYYMMDD
+  right: literal('?'),
+  secType: AssetClass.extract(['BOND']),
+  strike: literal(0),
+  ticker: string(),
+  validExchanges: string(),
+})
+
+export interface BondSecurityInfo extends GuardType<typeof BondSecurityInfo> {}
+
+export const CashSecurityInfo = props({
+  companyName: string(),
+  conid: number(),
+  currency: Currency3,
+  exchange: ExchangeCode,
+  listingExchange: ExchangeCode,
+  priceRendering: number(),
+  right: literal('?'),
+  secType: AssetClass.extract(['CASH']),
+  strike: literal(0),
+  ticker: string(),
+  validExchanges: string(),
+})
+
+export interface CashSecurityInfo extends GuardType<typeof CashSecurityInfo> {}
+
+export const FutureSecurityInfo = props({
+  companyName: string(),
+  conid: number(),
+  currency: Currency3,
+  exchange: ExchangeCode,
+  maturityDate: pattern(/^\d{8}$/), // YYYYMMDD
+  right: literal('?'),
+  secType: AssetClass.extract(['FUT']),
+  showPrips: boolean(),
+  strike: literal(0),
+  ticker: string(),
+  validExchanges: string(),
+})
+
+export interface FutureSecurityInfo extends GuardType<typeof FutureSecurityInfo> {}
+
+export const StockContractInfo = props({
+  companyName: string(),
+  conid: number(),
+  currency: Currency3,
+  exchange: ExchangeCode,
+  listingExchange: ExchangeCode,
+  right: literal('?'),
+  secType: AssetClass.extract(['STK']),
+  strike: literal(0),
+  ticker: string(),
+  validExchanges: string(),
+})
+
+export interface StockContractInfo extends GuardType<typeof StockContractInfo> {}
+
+export const SecurityInfo = union([BondSecurityInfo, CashSecurityInfo, FutureSecurityInfo, StockContractInfo])
+
+export type SecurityInfo = GuardType<typeof SecurityInfo>
 
 export class Info {
   readonly #client: InteractiveBrokersResourceClient
@@ -21,7 +98,7 @@ export class Info {
 
     /** Expiration month for the given derivative. */
     // todo probably only relevant for options and futures
-    readonly month?: undefined | never // todo
+    readonly month?: undefined | string // todo
 
     /** Designate the exchange you wish to receive information for in relation to the contract */
     readonly exchange?: undefined | ExchangeCode
@@ -47,12 +124,14 @@ export class Info {
   }, { signal, timeout }: {
     readonly signal?: undefined | AbortSignal
     readonly timeout?: undefined | number
-  } = {}): Promise<unknown> {
+  } = {}): Promise<SecurityInfo> {
     return await this.#client.get({
       searchParams: parameters,
-      guard: undefined, // todo write a guard for this
+      guard: SecurityInfo, // todo write a guard for this
       signal,
       timeout,
     })
   }
+
+  // TODO getByAssetClass
 }
